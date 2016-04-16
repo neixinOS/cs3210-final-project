c8c8832f0ff9299ae4da3d8b16be468b1811206a
Jing Hong <jhong84@gatech.edu>
2016-04-16 15:22:32 -0400

    transmit

---
+++ lab4a/kern/monitor.h
@@ -0,0 +1,26 @@
+#ifndef JOS_KERN_MONITOR_H
+#define JOS_KERN_MONITOR_H
+#ifndef JOS_KERNEL
+# error "This is a JOS kernel header; user programs should not #include it"
+#endif
+
+struct Trapframe;
+
+// Activate the kernel monitor,
+// optionally providing a trap frame indicating the current state
+// (NULL if none).
+void monitor(struct Trapframe *tf);
+
+// Functions implementing monitor commands.
+int mon_help(int argc, char **argv, struct Trapframe *tf);
+int mon_infokern(int argc, char **argv, struct Trapframe *tf);
+int mon_infopg(int argc, char **argv, struct Trapframe *tf);
+int mon_backtrace(int argc, char **argv, struct Trapframe *tf);
+
+// Functions implementing extended monitor commands for Lab2.
+int mon_showmappings(int argc, char **argv, struct Trapframe *tf);
+int mon_setmapping(int argc, char **argv, struct Trapframe *tf);
+int mon_dumpcontents(int argc, char **argv, struct Trapframe *tf);
+int mon_pagedirectory(int argc, char **argv, struct Trapframe *tf);
+
+#endif  // !JOS_KERN_MONITOR_H
